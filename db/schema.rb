e0a487ab4bfa2fc0907ad1f37d7179b97a947a0d
davegreene2018 <davegreene2017@gmail.com>
2020-03-30 00:06:45 +0100

    email reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_211553) do
+ActiveRecord::Schema.define(version: 2020_03_29_230539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_211553) do
     t.datetime "comfirmed_at"
     t.datetime "confirmation_sent_at"
     t.datetime "confirmed_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
