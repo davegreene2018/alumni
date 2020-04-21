eae6de619280b4b476220acf9e7df24c5c981b80
davegreene2018 <davegreene2017@gmail.com>
2020-04-21 19:00:19 +0100

    CSS

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,10 +1,10 @@
 module ForumsHelper
 
-	def forum_author(forum)
+	def forum_is_owner(forum)
     user_signed_in? && current_user.id == forum.user_id
   end
 
-  def reply_author(reply)
+  def reply_is_owner(reply)
     user_signed_in? && current_user.id == reply.user_id
   end
 
