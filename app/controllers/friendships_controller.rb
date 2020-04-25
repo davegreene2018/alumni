55a66f773f8f818399f4901e8f76a1db61e0669c
davegreene2018 <davegreene2017@gmail.com>
2020-04-25 14:56:38 +0100

    final

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
       flash[:notice] = "Added friend."
-      redirect_to profiles_path
+      redirect_to request.referrer
     else
       flash[:error] = "Error occurred when adding friend."
       redirect_to profiles_path
@@ -14,6 +14,6 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
     flash[:notice] = "Successfully destroyed friendship."
-    redirect_to profiles_path
+    redirect_to request.referrer
   end
 end
